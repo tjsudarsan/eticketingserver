1b05016b0e1058427fb6e97d46dfdc2ffd36fcc6
tjsudarsan <tjsudarsan@gmail.com>
2018-01-02 10:43:14 +0530

    added checking of aadhaar in database

--- server.js
+++ server.js
@@ -2,65 +2,37 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var MongoClient = require('mongodb').MongoClient
-
-var un = 'test';
-var pass = 'test'
-var url = `mongodb://${un}:${pass}@ds239127.mlab.com:39127`
-
- MongoClient.connect(url, function(err,client) {
-if(err) throw err
-
-var db = client.db('mtcticketing') 
-db.collection('aadhaar').find().toArray(function(err,result){
-    if (err) throw err
-    console.log(result);
-    
-})
- })
+var MongoClient = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.get('/home',(req,res,err)=>{
-//     var data = [
-//         {
-//             name: 'sri',
-//             age: 21,
-//             city: 'chennai'
-//         },
-//         {
-//             name: 'tjs',
-//             age: 21,
-//             city: 'kpm'
-//         }
-//     ];
-//     res.send(JSON.stringify(data));
-// });
-
-app.post('/loginservice',(request,response,err)=>{
-    console.log(request.body);
-    var array = request.body;
-
-    var strings = array.map((item)=>{
-        return (
-            `my name is ${item.name}`
-        )
-    })
-
-    response.send(strings);
-    
+var url = `mongodb://root:root@mtcticketing-shard-00-00-jfxfq.mongodb.net:27017,mtcticketing-shard-00-01-jfxfq.mongodb.net:27017,mtcticketing-shard-00-02-jfxfq.mongodb.net:27017/test?ssl=true&replicaSet=mtcticketing-shard-0&authSource=admin`;
+var db = null;
+MongoClient.connect(url, function (err, client) {
+    if (err) throw err
+    db = client.db('mtcticketing')
+    console.log('database connected')
 })
 
-app.post('/checkaadhaar',(request,response,err)=>{
-    console.log(request.body.uid);
-    var uniqueIdentity = request.body.uid;
+app.post('/checkaadhaar', (request, response, err) => {
+    db.collection('aadhaar').find({ uid: request.body.uid }).toArray((err, result) => {
+        if (err) throw err
+        console.log(result, request.body.uid);
 
-    response.send(JSON.stringify({
-        status:true
-    }));
-    })
+        if (result.length === 1) {
+            response.send(JSON.stringify({
+                status: true
+            }));
+        }
+        else {
+            response.send(JSON.stringify({
+                status: false
+            }));
+        }
+    });
+})
 
-app.listen(4000,'192.168.1.7',()=>{
+app.listen(4000, () => {
     console.log('server started');
 })
